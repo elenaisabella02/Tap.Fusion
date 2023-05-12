83e8e8976abb465818eed397471be0abc789043f
Braga Elena-Isabella <student6@academydevopsoutlook.onmicrosoft.com>
2023-05-12 11:45:12 +0300

    Added base files to project

---
+++ Tap.Mocking/Tap.Mocking.Domain/StudentService.cs
@@ -0,0 +1,24 @@
+﻿using Tap.Mocking.Repository;
+
+namespace Tap.Mocking.Domain
+{
+    public class StudentService
+    {
+        private readonly IStudentRepository studentRepository;
+
+        public StudentService(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+        public IReadOnlyCollection<Student> GetStudents()
+        {
+            return this.studentRepository.GetAll();
+        }
+
+        public IReadOnlyCollection<Student> GetStudentsInFinalYear()
+        {
+            return this.studentRepository.GetAll().Where(student => student.CurrentStudyYear == 3).ToList();
+        }
+    }
+}
