090f52f9d27f168eefb82a25609ba0348dc18f2c
Braga Elena-Isabella <student6@academydevopsoutlook.onmicrosoft.com>
2023-05-16 17:15:40 +0300

    Added first reservation as test in calendar

---
+++ Tap.Mocking/Tap.Fusion/Server/Controllers/AppDataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tap.Fusion.Shared;
+
+namespace Tap.Fusion.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AppDataController : ControllerBase
+    {
+        [HttpGet]
+        public List<AppData> Get()
+        {
+            List<AppData> dataSource = new List<AppData>
+            {
+                new AppData { Id = 1, Subject = "Reserved", StartTime = new DateTime(2023, 5, 11, 9, 30, 0), EndTime = new DateTime(2023, 5, 11, 11, 0, 0) }
+            };
+            return dataSource;
+        }
+    }
+}
---
+++ Tap.Mocking/Tap.Fusion/Shared/AppData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tap.Fusion.Shared;
+
+namespace Tap.Fusion.Shared
+{
+
+    public class AppData
+    {   
+        public int Id { get; set; }
+        public string? Subject { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
