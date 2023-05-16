0e8864ba4645ddc5e8f92d7af882ea6069515b4c
Braga Elena-Isabella <student6@academydevopsoutlook.onmicrosoft.com>
2023-05-16 16:56:08 +0300

    Added scheduler in rent-a-bike page

--- Tap.Mocking/Tap.Fusion/Client/Program.cs
+++ Tap.Mocking/Tap.Fusion/Client/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tap.Fusion.Client;
+using Syncfusion.Blazor;
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTk5MDA2MkAzMjMxMmUzMjJlMzNLS3V4ak5FeG1GRXhhcUxLT0JyKzFVV1RrK1NRaDc0U1QyU2FoVXhnQkw4PQ==");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.Services.AddSyncfusionBlazor();
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
